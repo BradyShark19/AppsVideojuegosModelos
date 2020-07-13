021070103c93b270e14633345d3636186809ca0f
BradyShark19 <bradyraule@gmail.com>
2020-07-12 22:27:21 -0500

    Sincronizando Proyecto...

---
+++ CuboV1/Assets/Scripts/CuerpoControl.cs
@@ -0,0 +1,20 @@
+﻿/*Librerias*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*Clase Principal*/
+public class CuerpoControl : MonoBehaviour
+{/*Variables*/
+    public Joystick S1;
+    public Joystick S2;
+    public float Velocidad;
+    public float VelocidadAngular;
+    /*Metodos*/
+    void Update()
+    {
+        transform.localPosition += Time.deltaTime * new Vector3(S1.Horizontal, 0, S1.Vertical) * Velocidad;
+        transform.Rotate(Vector3.up, Time.deltaTime * S2.Horizontal * VelocidadAngular);
+    }
+}
---
+++ CuboV1/Assets/Scripts/Joystick.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Joystick : MonoBehaviour, IDragHandler, IEndDragHandler
+{ /*Variables*/
+    public Canvas Padre;
+    public float Radio;
+    private Vector2 Axis;
+
+    Vector3 PosInicial;
+
+    /*Metodos*/
+
+    public Vector2 axis
+    {
+        get { return Axis; }
+    }
+
+    public float Horizontal
+    {
+        get { return Axis.x; }
+    }
+
+    public float Vertical
+    {
+        get { return Axis.y; }
+    }
+
+    public bool IsMoving
+    {
+        get { return Axis != Vector2.zero; }
+    }
+
+    void Start()
+    {
+        PosInicial = transform.position;
+    }
+
+    public void OnDrag(PointerEventData point)
+    {
+        Vector2 Pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(Padre.transform as RectTransform, point.position, Padre.worldCamera, out Pos);
+        Vector2 NewPos = Padre.transform.TransformPoint(Pos) - PosInicial;
+        NewPos.x = Mathf.Clamp(NewPos.x, -Radio, Radio);
+        NewPos.y = Mathf.Clamp(NewPos.y, -Radio, Radio);
+
+        Axis = NewPos / Radio;
+
+        transform.localPosition = NewPos;
+    }
+
+    public void OnEndDrag(PointerEventData point)
+    {
+        transform.position = PosInicial;
+        Axis = Vector2.zero;
+    }
+
+
+}
+
